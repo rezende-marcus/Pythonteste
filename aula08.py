479c93233de139962aadb4256713d123021cfe75
rezende-marcus <mglrezende@yahoo.com.br>
2021-05-14 16:20:08 -0300

    aula08

---
+++ aula08.py
@@ -0,0 +1,8 @@
+import emoji
+#from math import sqrt, floor
+#import math
+#num = int(input('Digite um número: '))
+#raiz = sqrt(num)
+#raiz = math.sqrt(num)
+#print('A raiz de {} é igual a {:.2f}'.format(num, raiz))
+#print('A raiz de {} é igual a {:.2f}'.format(num, floor(raiz)))
