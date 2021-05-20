19f2e91112e5d8dfb74397f781babc00a8affd78
rezende-marcus <mglrezende@yahoo.com.br>
2021-05-20 10:59:12 -0300

    aula09

---
+++ aula09.py
@@ -0,0 +1,10 @@
+frase = 'Curso em Vídeo Python'
+print(frase[::2])
+
+print('Oi')
+
+print("""hkjhhasd ljhdashjlkçhA ÇLLJLJLJDSA MKLJHLJKLJ
+JLKAJLKJljLJLKJKLJ   HJLKHKlhH      JHhHhJK
+LJLKJLKJKJLKJJK""")
+
+
