74add14d396de416d8447ce68b0221e5f44fe861
rezende-marcus <mglrezende@yahoo.com.br>
2021-05-21 11:33:06 -0300

    aula12

---
+++ aula11.py
@@ -0,0 +1,7 @@
+'''nome = 'Guanabara'
+cores = {}
+print('Olá! Muito prazer em te conhecer, {}{}{}!!!'.format('\033[4;34m', nome, '\033[m'))'''
+n = 19 // 2
+b = 19 % 2
+print('{} {}'.format(n, b))
+
