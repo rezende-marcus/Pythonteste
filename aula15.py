f9df889e4974d1fec6d801b2e7d7d12a9c72b805
rezende-marcus <mglrezende@yahoo.com.br>
2021-06-01 14:33:27 -0300

    Create aula15.py

---
+++ aula15.py
@@ -0,0 +1,22 @@
+nome = 'Jose'
+idade = 33
+salario = 987.3
+print(f'O {nome} tem {idade} anos e ganha R${salario:.2f}')
+print('O {} tem {} anos.'.format(nome, idade))
+
+
+
+'''cont = 1
+while cont <= 10:
+    print(cont, '-> ', end='')
+    cont += 1
+print('Acabou')'''
+'''n = s = 0
+while True:
+    n = int(input('Digite um número: '))
+    if n == 999:
+        break
+    s += n
+#print('A soma vale {}'.format(s))
+print(f'A soma vale {s}')'''
+
