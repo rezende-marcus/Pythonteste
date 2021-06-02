259157200ebcd15851ff62e11d3f9c82cd45230a
rezende-marcus <mglrezende@yahoo.com.br>
2021-06-02 09:15:35 -0300

    Create aula16.py

---
+++ aula16.py
@@ -0,0 +1,13 @@
+lanche = ('Hamburger', 'Suco', 'Pizza', 'Pudim', 'batata frita')
+#Tuplas são imutáveis
+#print(len(lanche))
+'''for cont in range(0, len(lanche)):
+    print(f'Eu vou comer {lanche[cont]}')'''
+
+'''for comida in lanche:
+    print(f'Eu vou comer {comida}')'''
+
+for pos, comida in enumerate(lanche):
+    print(f'Eu vou comer {comida} na posição {pos}')
+
+print('Comi pra caramba')
