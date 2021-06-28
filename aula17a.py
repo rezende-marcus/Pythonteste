e1898fbf2a9f63b8854c315225613b5cfaf4b2ef
rezende-marcus <mglrezende@yahoo.com.br>
2021-06-28 15:52:12 -0300

    Create aula17a.py

---
+++ aula17a.py
@@ -0,0 +1,31 @@
+a = [2, 3, 4, 7]
+b = a[:] # copia da lista a
+b[2] = 8 # troca a posição 2 que e num 4 por num 8
+print(f'Lista A: {a}')
+print(f'Lista B: {b}')
+
+#valores = list()
+'''valores.append(5)
+valores.append(9)
+valores.append(4)'''
+'''for cont in range(0, 5):
+    valores.append(int(input('Digite um valor: ')))
+
+for c, v, in enumerate(valores): # c = corresponde as chaves
+    print(f'Na posição {c} encontrei o valor {v}!')
+print('Cheguei ao final da lista.')'''
+
+
+
+'''num = [2, 5, 9, 1]
+num[2] = 3
+num.append(7)
+num.sort(reverse=True)
+num.insert(2, 2)
+if 4 in num:
+    num.remove(4)
+else:
+    print('Não achei o numero 4')
+#num.pop(2)
+print(num)
+print(f'Essa lista tem {len(num)} elementos.')'''
